0c1722a9a29205f4e3780ea7233245b10e8d84a8
SO Vengleab <sovengleab@gmail.com>
2026-02-15 10:00:03 +0700

    feat: Implement file upload endpoint in FastAPI and add web scraping Jupyter notebook

--- 05-fast-api/routes/users.py
+++ 05-fast-api/routes/users.py
@@ -1,7 +1,13 @@
 import fastapi
+from fastapi import File, UploadFile
 
 userRoutes = fastapi.APIRouter()
 
 @userRoutes.get("/users")
 def get_users():
     return {"message": "Hello World"}
+
+
+@userRoutes.get("/fileupload")
+def fileupload(file: UploadFile = File(...)):
+    return {"message": "File uploaded successfully", "file_name": file.filename}
